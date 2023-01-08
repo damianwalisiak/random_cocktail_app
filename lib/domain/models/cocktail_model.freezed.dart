b3e955e99b6a262e3c08774ffd7e8991e84e621a
Damian Walisiak <dwalisiak@gmail.com>
2023-01-09 00:36:10 +0100

    Final run build_runner build

--- lib/domain/models/cocktail_model.freezed.dart
+++ lib/domain/models/cocktail_model.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
 
 part of 'cocktail_model.dart';
 
@@ -220,7 +220,6 @@ class _$_CocktailModel implements _CocktailModel {
   @override
   @IngredientsConverter()
   List<IngredientModel> get ingredientsList {
-    if (_ingredientsList is EqualUnmodifiableListView) return _ingredientsList;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableListView(_ingredientsList);
   }
